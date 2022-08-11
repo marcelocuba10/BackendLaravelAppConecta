21b5619a7505a1bcbf0c71029c43d55ab05c7bd4
Marcelo Cuba <marcelo.cuba.10@hotmail.com>
2022-08-11 13:28:03 -0400

    FIX alert modal message in module Admin & User

--- Modules/User/Resources/views/layouts/includes/alerts.blade.php
+++ Modules/User/Resources/views/layouts/includes/alerts.blade.php
@@ -1,52 +1,46 @@
 @if ($errors->any())
     @foreach ($errors->all() as $error)
-        <div style="position: absolute; top: 65px; right: 20px; z-index:9999">
-            <div class="toast toast1 toast-bootstrap fade panel-warning show" role="alert" aria-live="assertive" aria-atomic="true">
-                <div class="toast-header panel-heading" style="padding: 5px">
-                    <i class="fa fa-info-circle"> </i>
-                    <strong class="mr-auto m-l-sm"> Aviso</strong>
-                    <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="toast-body">
-                    {{ $error }}
-                </div>
+        <div style="position: absolute; top: 119px; right: 20px; z-index:9999">
+            <div class="alert alert-danger d-flex align-items-center alert-dismissible fade show" role="alert">
+                <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                    <path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z" />
+                </svg>
+                <div style="font-size: 15px;margin-left: 10px;">{{ $error }}</div>
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
             </div>
-        </div>  
+        </div>    
     @endforeach
 @endif
 
 @if (session('message'))
-    <div style="position: absolute; top: 65px; right: 20px; z-index:9999">
-        <div class="toast toast1 toast-bootstrap fade panel-info show" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header panel-heading" style="padding: 5px">
-                <i class="fa fa-info-circle"> </i>
-                <strong class="mr-auto m-l-sm"> Aviso</strong>
-                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="toast-body">
-                {{ session('message') }}
-            </div>
+    <div style="position: absolute; top: 119px; right: 20px; z-index:9999">
+        <div class="alert alert-success d-flex align-items-center alert-dismissible fade show" role="alert">
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M12 2C6.5 2 2 6.5 2 12S6.5 22 12 22 22 17.5 22 12 17.5 2 12 2M12 20C7.59 20 4 16.41 4 12S7.59 4 12 4 20 7.59 20 12 16.41 20 12 20M16.59 7.58L10 14.17L7.41 11.59L6 13L10 17L18 9L16.59 7.58Z" />
+            </svg>
+            <div style="font-size: 15px;margin-left: 10px;">{{ session('message') }}</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
     </div>        
 @endif
 
 @if (session('error'))
-    <div style="position: absolute; top: 65px; right: 20px; z-index:9999">
-        <div class="toast toast1 toast-bootstrap fade panel-danger show" role="alert" aria-live="assertive" aria-atomic="true">
-            <div class="toast-header panel-heading" style="padding: 5px">
-                <i class="fa fa-info-circle"> </i>
-                <strong class="mr-auto m-l-sm"> Aviso</strong>
-                <button type="button" class="ml-2 mb-1 close" data-dismiss="toast" aria-label="Close">
-                    <span aria-hidden="true">×</span>
-                </button>
-            </div>
-            <div class="toast-body">
-                {{ session('error') }}
-            </div>
+    <div style="position: absolute; top: 119px; right: 20px; z-index:9999">
+        <div class="alert alert-danger d-flex align-items-center alert-dismissible fade show" role="alert">
+            <svg style="width:24px;height:24px" viewBox="0 0 24 24">
+                <path fill="currentColor" d="M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2C6.47,2 2,6.47 2,12C2,17.53 6.47,22 12,22C17.53,22 22,17.53 22,12C22,6.47 17.53,2 12,2M14.59,8L12,10.59L9.41,8L8,9.41L10.59,12L8,14.59L9.41,16L12,13.41L14.59,16L16,14.59L13.41,12L16,9.41L14.59,8Z" />
+            </svg>
+            <div style="font-size: 15px;margin-left: 10px;">{{ session('message') }}</div>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-    </div>        
+    </div>      
 @endif
+
+<script>
+$(document).ready(function() {
+    // show the alert
+    setTimeout(function() {
+        $(".alert-success").alert('close');
+    }, 2500);
+});
+</script>
